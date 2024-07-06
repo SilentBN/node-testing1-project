56faf4919a1246d11ddddd9dd96614ce07c9bf1b
SilentBN <61912362+SilentBN@users.noreply.github.com>
2024-07-06 11:37:51 -0400

    Implement trimProperties function and corresponding tests

--- index.js
+++ index.js
@@ -8,6 +8,15 @@
  */
 function trimProperties(obj) {
   // ✨ implement
+  const trimmedObj = {};
+  for (let key in obj) {
+    if (typeof obj[key] === "string") {
+      trimmedObj[key] = obj[key].trim();
+    } else {
+      trimmedObj[key] = obj[key];
+    }
+  }
+  return trimmedObj;
 }
 
 /**
@@ -93,8 +102,8 @@ class Car {
    * @param {number} mpg - miles the car can drive per gallon of gas
    */
   constructor(name, tankSize, mpg) {
-    this.odometer = 0 // car initilizes with zero miles
-    this.tank = tankSize // car initiazes full of gas
+    this.odometer = 0; // car initilizes with zero miles
+    this.tank = tankSize; // car initiazes full of gas
     // ✨ initialize whatever other properties are needed
   }
 
@@ -156,4 +165,4 @@ module.exports = {
   Counter,
   Seasons,
   Car,
-}
+};
